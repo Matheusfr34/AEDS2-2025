09d9a0e229eb1e2eaad7400519d7640d5390826e
Matheusfr34 <164033400+Matheusfr34@users.noreply.github.com>
2025-03-03 13:09:14 -0300

    ex03 do tp01

---
+++ ex03/Ciframento.java
@@ -0,0 +1,40 @@
+package ex03;
+
+import java.util.Scanner;
+
+public class Ciframento {
+
+    public static String cifrar(String fraseOriginal){
+        char[] fraseCifrada = new char[fraseOriginal.length()];
+
+        for(int i = 0; i < fraseOriginal.length(); i++){
+            char caracterOriginal = fraseOriginal.charAt(i);
+
+            if(caracterOriginal >= 32 && caracterOriginal <= 126){
+                char caracterCifrado = (char) (caracterOriginal + 3);
+                
+                if(caracterCifrado > 126){
+                    caracterCifrado = (char) (caracterCifrado - 95);
+                }
+                fraseCifrada[i] = caracterCifrado;
+            } else {
+                fraseCifrada[i] = caracterOriginal;
+            }
+        }
+
+        return new String(fraseCifrada);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean flag = false;
+        while(!flag){
+            String palavra = sc.nextLine();
+            if(palavra.charAt(0) == 'F' && palavra.charAt(1) == 'I' && palavra.charAt(2) == 'M'){
+                flag = true;
+            }
+            String palavraCifrada = cifrar(palavra);
+            System.out.println(palavraCifrada);
+        }
+        sc.close();
+    }
+}
