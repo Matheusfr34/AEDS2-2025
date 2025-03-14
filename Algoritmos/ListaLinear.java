ef597f9dc453d9662769563ee8e491637553a811
Matheusfr34 <164033400+Matheusfr34@users.noreply.github.com>
2025-03-14 18:22:23 -0300

    Update ListaLinear.java

--- Algoritmos/ListaLinear.java
+++ Algoritmos/ListaLinear.java
@@ -49,7 +49,19 @@ class Lista {
     }
 
     public int removerInicio(){
-        
+        if(n == 0){
+            System.out.println("ERRO. LISTA CHEIA");
+            return 0;
+        }
+
+        n--;
+        int resp = array[0];
+
+        for(int i = 0; i < n; i++){
+            array[i] = array[i + 1];
+        }
+
+        return resp;
     }
 
     public int removerFim(){
@@ -59,6 +71,84 @@ class Lista {
         }
         return array[--n];
     }
+
+    public int removerPos(int pos){
+        if(n == 0 || pos < 0 || pos >= n){
+            System.out.println("ERRO. LISTA CHEIA");
+            return 0;
+        }
+        n--; 
+        int resp = array[pos];
+        for(int i = pos; i < n; i++){
+            array[i] = array[i + 1];
+        }
+
+        return resp;
+    }
+
+    public void mostrar(){
+        System.out.println();
+        System.out.print("[ ");
+        for(int i = 0; i < n; i++){
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("]");
+    }
+
+    //Exercício 1 - implemente um método que retorna a soma dos elementos contidos na mesma. 
+    public int somar(){
+        int soma = 0;
+        for(int i = 0; i < n; i++){
+            soma += array[i];
+        }
+        return soma;
+    }
+
+    //Exercício 2 - implemente um método que retorna o maior elemento contido na mesma. 
+    public int maior(){
+        int maior = array[0];
+        for(int i = 1; i < n; i++){
+            if(array[i] > maior){
+                maior = array[i];
+            }
+        }
+        return maior;
+    }
+
+    //Exercício 3 - implemente um método que inverte a ordem dos seus elementos. 
+    public void inverter(){
+        for(int i = 0; i < n/2; i++){
+            int tmp = array[i];
+            array[i] = array[n - i - 1];
+            array[n - i - 1] = tmp;
+        }
+    }
+
+    //Exercicio 4 - implemente um método que retorna o número de elementos pares and múltiplos de cinco contidos na mesma.
+    public int paresAndMultiplos(){
+        int contador = 0;
+        for(int i = 0; i < n; i++){
+            if(array[i] % 2 == 0 && array[i] % 5 == 0){
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    //Exercício 5 - Modifique nossa lista linear, criando uma lista ordenada. 
+    public void ordenarInsercao(){
+        for(int i = 1; i < n; i++){
+            int tmp = array[i];
+            int j = i - 1;
+            while(j >= 0 && array[j] > tmp){
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = tmp;
+        }
+    }
+
+
 }
 
 public class ListaLinear {
@@ -67,10 +157,19 @@ public class ListaLinear {
         int tamanho = sc.nextInt();
         Lista lista = new Lista(tamanho);
 
-        lista.inserirInicio(3);
-        lista.inserirInicio(4);
-        lista.inserirInicio(5);
-        lista.inserirInicio(7);
+        int valores[] = new int[tamanho];
+        for(int i = 0; i < valores.length; i++){
+            valores[i] = sc.nextInt();
+            lista.inserirFim(valores[i]);
+        }
+
+        lista.mostrar();
+        System.out.println();
+        System.out.println("SOMA DOS NÚMEROS: " + lista.somar());
+        System.out.println("MAIOR: " + lista.maior());
+        System.out.println("QUANT. PARES E MÚLTIPLOS DE 5: " + lista.paresAndMultiplos());
+        lista.ordenarInsercao();
+        lista.mostrar();
 
         sc.close();
     }
