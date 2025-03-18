048644f9b26d4937278d23d9915379006a8e1529
Matheusfr34 <164033400+Matheusfr34@users.noreply.github.com>
2025-03-17 21:09:13 -0300

    Create FilaCircularLinear.java

---
+++ Algoritmos/FilaCircularLinear.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+class FilaCircular{
+    int[] array;
+    int primeiro, ultimo;
+
+    //Construtor que cria a fila circular e aponta os ponteiros primeiro e ultimo para 0
+    public FilaCircular(int tam){
+        array = new int[tam + 1];
+        primeiro = ultimo = 0;
+    }
+
+    //Método para inserir um elemento na fila (enqueue ou enfileirar)
+    public void enfileirar(int elemento){
+        if(((ultimo + 1) % array.length) == primeiro){
+            System.out.println("Erro");
+            return;
+        }
+
+        array[ultimo] = elemento;
+        ultimo = (ultimo + 1) % array.length;
+    }
+
+    //Método para remover um elemento na fila (dequeue ou desenfileirar)
+    public int desenfileirar(){
+        if(primeiro == ultimo){
+            System.out.println("Erro");
+            return 0;
+        }
+
+        int resp = array[primeiro];
+        primeiro = (primeiro + 1) % array.length;
+
+        return resp;
+    }
+
+    //Método para mostrar os elementos presentes na fila 
+    public void mostrar(){
+        int i = primeiro;
+        System.out.print("[ ");
+        while(i != ultimo){
+            System.out.print(array[i] + " ");
+            i = (i + 1) % array.length;
+        }
+        System.out.println("]");
+    }
+
+    //Método para pesquisar se um elemento está presente na fila circular 
+    public boolean pesquisar(int elemento){
+        int i = primeiro;
+        while(i != ultimo){
+            if(array[i] == elemento){
+                return true;
+            }
+            i = (i + 1) % array.length;
+        }
+        return false;
+    }
+
+    //Método que verifica se uma fila circular está vazia
+    public boolean isVazia(){
+        
+    }
+}
+
+public class FilaCircularLinear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int tam = sc.nextInt();
+
+        FilaCircular fila = new FilaCircular(tam);
+
+        for(int i = 0; i < tam; i++){
+            int valor = sc.nextInt();
+            fila.enfileirar(valor);
+        }
+
+        fila.mostrar();
+
+        sc.close();
+    }
+}
