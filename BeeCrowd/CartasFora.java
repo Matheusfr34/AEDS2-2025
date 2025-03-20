ddf99b3a5ac3eb8aab3ca0b92cf3360ae11e8161
Matheusfr34 <164033400+Matheusfr34@users.noreply.github.com>
2025-03-20 10:46:00 -0300

    em processo

---
+++ BeeCrowd/CartasFora.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class CartasFora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int entrada = sc.nextInt();
+        while(entrada != 0){
+            entrada = sc.nextInt();
+        }
+        sc.close();
+    }
+}
